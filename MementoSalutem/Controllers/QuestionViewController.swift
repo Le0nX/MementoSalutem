9146a947b8e526bee4e00b8bb638f92bb5454716
Le0nX <cloudmindone@gmail.com>
2019-02-23 17:40:19 +0300

    feature: added first working functionality

--- MementoSalutem/Controllers/QuestionViewController.swift
+++ MementoSalutem/Controllers/QuestionViewController.swift
@@ -18,7 +18,8 @@ public class QuestionViewController: UIViewController {
     
     /**
      # Описание:
-     
+     Выставляем проверку на доуступ к вьюхе. Если кто-то по неосторожности
+     попытается к ней обратиться до ее загрузки, то получит nil
      */
     public var questionView: QuestionView! {
         guard isViewLoaded else {return nil}
@@ -27,9 +28,45 @@ public class QuestionViewController: UIViewController {
     
     override public func viewDidLoad() {
         super.viewDidLoad()
+        showQuestion()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func toggleAnswerLabels(_ sender: Any) {
+        questionView.answerLbl.isHidden = !questionView.answerLbl.isHidden
+        questionView.hintLbl.isHidden = !questionView.hintLbl.isHidden
+    }
     
+    private func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+        
+        questionView.answerLbl.text = question.answer
+        questionView.promtLbl.text = question.prompt
+        questionView.hintLbl.text = question.hint
+        
+        questionView.answerLbl.isHidden = true
+        questionView.hintLbl.isHidden = true
+    }
+    
+    @IBAction func handleIncorrect(_ sender: UIButton) {
+        incorrectCount += 1
+        questionView.incorrecCountLbl.text = "\(incorrectCount)"
+        showNextQuestion()
+    }
+    
+    @IBAction func handleCorrect(_ sender: UIButton) {
+        correctCount += 1
+        questionView.correctCountLbl.text = "\(correctCount)"
+        showNextQuestion()
+    }
+    
+    private func showNextQuestion() {
+        questionIndex += 1
+        guard questionIndex < questionGroup.questions.count else {
+            // handle
+            return
+        }
+        showQuestion()
+    }
 }
 
