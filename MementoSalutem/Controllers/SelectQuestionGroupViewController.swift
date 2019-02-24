0a8c6f286db72171c82d5bc7a84c291175202711
Le0nX <cloudmindone@gmail.com>
2019-02-24 20:15:39 +0300

    feature: set the delegate

--- MementoSalutem/Controllers/SelectQuestionGroupViewController.swift
+++ MementoSalutem/Controllers/SelectQuestionGroupViewController.swift
@@ -14,6 +14,8 @@ public class SelectQuestionGroupViewController: UIViewController {
     @IBOutlet internal var tableView: UITableView! {
         didSet {
             /// трюк, чтобы UITableView не отрисовывал пустые ячейки, после того, как отрисовал все нужные ячейки
+            tableView.dataSource = self
+            tableView.delegate = self
             tableView.tableFooterView = UIView()
         }
     }
@@ -34,6 +36,22 @@ extension SelectQuestionGroupViewController: UITableViewDataSource {
         cell.titleLbl.text = questionGroup.title
         return cell
     }
+}
+
+extension SelectQuestionGroupViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    /**
+     This sets the selectedQuestionGroup to the one that was selected. You have to do this here instead of in tableView(_:, didSelectRowAt:), because didSelectRowAt: is triggered after the segue is performed. If you set selectedQuestionGroup in didSelectRowAt: then the app would crash on the line viewController.questionGroup = selectedQuestionGroup as selectedQuestionGroup would still be nil.
+     */
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionGroups[indexPath.row]
+        return indexPath
+    }
     
-    
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else {return}
+        viewController.questionGroup = selectedQuestionGroup
+    }
 }
